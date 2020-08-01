a6187b8f38c152959f606f201ddbb22232150e1e
unknown <lhs009@gmail.com>
2020-08-01 10:29:57 +0900

    printUtil.js update barcode string deleted and TAX FREE String added

--- public/js/lib/printerUtil.js
+++ public/js/lib/printerUtil.js
@@ -36,7 +36,7 @@ requestPrint(p_name.value, strSubmit, viewResult);
 
 */
 
-function printCodes(qrString, barString) {
+function _printCodes(qrString, barString) {
   let barCodeData = barString; // 바코드 데이터
   let barCodeSymbol = 1; // 바코드 타입 code 128
   let barCodeHeight = 100; // 바코드 높이
@@ -44,7 +44,7 @@ function printCodes(qrString, barString) {
   let barPosY = 140; // Y 좌표
   let barWidth = 3; // 바코드 길이
   let nBarWidth = 1;
-  let barCodeHri = 1; // 바코드에 나오는 글자 위치 및 사이즈 (1: 밑에 폰트 사이즈 1, 3: 밑에 폰트 사이즈 2)
+  let barCodeHri = 1; // 바코드에 나오는 글자 위치 및 사이즈 (0: 프리터 안함, 1: 밑에 폰트 사이즈 1, 3: 밑에 폰트 사이즈 2)
   let barRotation = 0; // rotation 없음
 
   setLabelId(issueID); // label_data['id'] = 1, label_data { id: 1, functions: {} }
@@ -67,12 +67,12 @@ function printCodes(qrString, barString) {
     barCodeHri
   );
 
-  // 1) data: 바코드 데이터
+  // 1) data: QR 데이터
   // 2) x: x축 좌표
   // 3) y: y축 좌표
   // 4) model: 0 , 1
   // 5) eccLevel: 에러 교정 레벨 값 (L,M,Q,H)
-  // 6) size: 바코드 크기(1~9)
+  // 6) size: QR 크기(1~9)
   // 7) rotation: 회전
   let qrPosX = 120;
   let qrPosY = barPosY + barCodeHeight + 80;
@@ -87,6 +87,66 @@ function printCodes(qrString, barString) {
 
   let strSubmit = getLabelData(); //label_data.functions = label_func
 
+  console.log(strSubmit); // label_data = { id: 1, functions: { 'func1': { checkLabelStagus: [] }, 'func2': { clearBuffer: [] }, 'func3': { printBuffer: [] }}}
+
+  issueID++;
+  requestPrint(printName, strSubmit, viewResult);
+}
+
+function printCodes(qrString, barString) {
+  let barCodeData = barString; // 바코드 데이터
+  let barCodeSymbol = 1; // 바코드 타입 code 128
+  let barCodeHeight = 100; // 바코드 높이
+  let barPosX = 100; // X 좌표
+  let barPosY = 140; // Y 좌표
+  let barWidth = 3; // 바코드 길이
+  let nBarWidth = 1;
+  let barCodeHri = 1; // 바코드에 나오는 글자 위치 및 사이즈 (0: 프리터 안함, 1: 밑에 폰트 사이즈 1, 3: 밑에 폰트 사이즈 2)
+  let barRotation = 0; // rotation 없음
+
+  setLabelId(issueID); // label_data['id'] = 1, label_data { id: 1, functions: {} }
+
+  checkLabelStatus(); //  프린터 상태 체크 label_func['func1'] = { checkLabelStatus: [] }
+  clearBuffer(); //  프린터 버퍼 초기화 lable_func['func2'] = { clearBuffer: [] }
+  // 1. 프리터 버퍼에 상륙허가증 레이블 그리기
+  drawDeviceFont('상륙허가증', 140, 30, 'a', 2, 2, 0, 0, 0, 0);
+  // 2. 프린터 버퍼에 BARCODE PASSPORT 레이블 그리기
+  drawDeviceFont('BARCODE PASSPORT', 90, 70, 'a', 2, 2, 0, 0, 0, 0);
+
+  // 3. 프린터 버퍼에 1차원 바코드 그리기
+  draw1DBarcode(
+    barCodeData,
+    barPosX,
+    barPosY,
+    barCodeSymbol,
+    nBarWidth,
+    barWidth,
+    barCodeHeight,
+    barRotation,
+    barCodeHri
+  );
+  // 4. 프린터 버퍼에 TAX FREE String 삽입
+  let taxFreeStringY = barPosY + barCodeHeight + 5;
+  drawDeviceFont('TAX FREE', 150, taxFreeStringY, 'a', 2, 2, 0, 0, 0, 0);
+
+  // 1) data: QR 데이터
+  // 2) x: x축 좌표
+  // 3) y: y축 좌표
+  // 4) model: 0 , 1
+  // 5) eccLevel: 에러 교정 레벨 값 (L,M,Q,H)
+  // 6) size: QR 크기(1~9)
+  // 7) rotation: 회전
+  let qrPosX = 120;
+  let qrPosY = barPosY + barCodeHeight + 80;
+  let qrModel = 1; // model 2
+  let qrEccLevel = 'Q';
+  let qrSize = 4;
+  let qrRotation = 0;
+  // 5  프린터 버퍼에 QR 코드 그리기
+  drawQRCode(qrString, qrPosX, qrPosY, qrModel, qrEccLevel, qrSize, qrRotation);
+  // 프린터 버퍼에 있는 데이터 출력 label_func['func3'] = { printBuffer: [] }
+  printBuffer();
+  let strSubmit = getLabelData(); //label_data.functions = label_func
   console.log(strSubmit); // label_data = { id: 1, functions: { 'func1': { checkLabelStagus: [] }, 'func2': { clearBuffer: [] }, 'func3': { printBuffer: [] }}}
 
   issueID++;
